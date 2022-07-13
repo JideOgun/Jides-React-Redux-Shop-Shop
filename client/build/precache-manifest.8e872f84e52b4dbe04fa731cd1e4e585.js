7bbb05b57956762f367fce22369b279fc9d14c30
JideOgun <JideOgun@github.com>
2022-07-13 17:46:02 -0500

    Module 22 homework set up - installed redux

---
+++ client/build/precache-manifest.8e872f84e52b4dbe04fa731cd1e4e585.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2827df2f95205ef1eba4667949510a0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32e051623a78e4ec1eaa",
+    "url": "/static/css/main.be2b856d.chunk.css"
+  },
+  {
+    "revision": "24c40cc5354616da34f8",
+    "url": "/static/js/2.9a5f0e5c.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.9a5f0e5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32e051623a78e4ec1eaa",
+    "url": "/static/js/main.940d475c.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
